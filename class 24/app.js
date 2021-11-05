2d2dda887d14b882b4bd47d1e39968a4b1652408
jawantechpk <jawantechpk@gmail.com>
2021-11-05 11:11:17 +0500

    11/5/21
    11/5/21

---
+++ class 24/app.js
@@ -0,0 +1,107 @@
+// alert("HELLO CLASS...")
+
+
+
+
+
+
+// for(var i = 0 ; i<=10 ; i++){
+//     console.log(i)
+// }
+
+
+
+// var myName = "Jaffar"
+
+// console.log(myName.toUpperCase())
+
+
+            ////STRING////
+
+
+
+// var arr = ["jaffar" , "aman" , "mubashir"]
+
+// var firstName = "jaffar Aman"
+
+// console.log(firstName[7])
+
+// var city = prompt("enter your city").toUpperCase();
+
+// console.log("user city: " + city )
+
+
+// if(city === "karachi"){
+//     alert("WELCOME")
+// }else{
+//     alert("your city is not found!")
+// }
+
+
+
+// var userCity = prompt("ENTER YOUR CITY").toLowerCase();
+// var cityArr = ["islamabad" , "lahore" , "multan" , "karachi"];
+
+
+
+// var isMatch = "no"
+
+// for(var i = 0 ; i < cityArr.length ; i++){
+
+//     if(userCity   === cityArr[i]){
+//         alert("WELCOME")
+//         isMatch = "yes"
+//     }
+// }
+
+// if(isMatch == "no"){
+//     alert("CITY IS NOT FOUND")
+// }
+
+// 'jaffar'
+
+// var firstName = prompt("ENTE YOUR FIRST NAME");
+// var firstChar = firstName.slice(0 , 1).toUpperCase()
+// var otherChar = firstName.slice(1).toLowerCase()
+// var capWord  = firstChar + otherChar 
+
+// console.log(capWord)
+
+// var firstName = prompt("ENTE YOUR FIRST NAME");
+// var firstName = "jaFFar"
+// firstName = firstName[0].toUpperCase() + firstName.slice(1).toLowerCase()
+
+// var firstName = "jaffar"
+// console.log(firstName.slice(0 , -1))
+
+
+
+// var month = prompt("enter month name");
+// // console.log(month)
+
+
+// // june
+// if(month.length > 3){
+//     month = month[0].toUpperCase() + month.slice(1,3).toLowerCase()
+//     alert(month)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
